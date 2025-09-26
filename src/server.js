633deb03f01117230950cbf8372855df251884d0
William Skogberg <skogberw@arcada.fi>
2025-09-26 13:25:00 +0300

    fixade cors

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(cors({
   methods: ["GET","POST","PATCH","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type","Authorization"]
 }));
-app.options("*", cors());
+app.options("/api/*", cors());
 
 app.use(express.json());
 
